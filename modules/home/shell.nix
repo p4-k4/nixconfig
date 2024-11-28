2401bd279a57df10f19372014d80f2d6a0cb7835
p4-k4 <p424p424@gmail.com>
2024-11-29 00:56:43 +1300

    feat: Enable vi mode in zsh with enhanced configuration

--- modules/home/shell.nix
+++ modules/home/shell.nix
@@ -3,38 +3,64 @@
 {
   programs.zsh = {
     enable = true;
-    autosuggestion.enable = true;
+    enableAutosuggestions = true;
     enableCompletion = true;
     
-    oh-my-zsh = {
-      enable = true;
-      plugins = [ "git" "macos" ];
-      theme = "robbyrussell";
-    };
+    # Enable vi mode
+    defaultKeymap = "viins";
+    initExtra = ''
+      # Vi mode settings
+      bindkey -v
+      export KEYTIMEOUT=1  # Reduce mode switch delay
 
-    plugins = [
-      {
-        name = "zsh-autosuggestions";
-        src = pkgs.zsh-autosuggestions;
+      # Use vim keys in tab complete menu
+      bindkey -M menuselect 'h' vi-backward-char
+      bindkey -M menuselect 'k' vi-up-line-or-history
+      bindkey -M menuselect 'l' vi-forward-char
+      bindkey -M menuselect 'j' vi-down-line-or-history
+
+      # Change cursor shape for different vi modes
+      function zle-keymap-select {
+        if [[ ''${KEYMAP} == vicmd ]] ||
+           [[ $1 = 'block' ]]; then
+          echo -ne '\e[1 q'
+        elif [[ ''${KEYMAP} == main ]] ||
+             [[ ''${KEYMAP} == viins ]] ||
+             [[ ''${KEYMAP} = "" ]] ||
+             [[ $1 = 'beam' ]]; then
+          echo -ne '\e[5 q'
+        fi
       }
-      {
-        name = "zsh-completions";
-        src = pkgs.zsh-completions;
-      }
-    ];
+      zle -N zle-keymap-select
+
+      # Use beam shape cursor on startup
+      echo -ne '\e[5 q'
+
+      # Use beam shape cursor for each new prompt
+      preexec() { echo -ne '\e[5 q' ;}
+    '';
 
     shellAliases = {
       ll = "ls -la";
       ".." = "cd ..";
+      "..." = "cd ../..";
+      "gst" = "git status";
+      "gco" = "git checkout";
+      "gcm" = "git commit -m";
+      "gaa" = "git add .";
+      "gp" = "git push";
+    };
+
+    oh-my-zsh = {
+      enable = true;
+      plugins = [
+        "git"
+        "docker"
+        "docker-compose"
+        "npm"
+        "vi-mode"  # Enable vi-mode plugin
+      ];
+      theme = "robbyrussell";
     };
   };
-
-  programs.autojump.enable = true;
-
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
-  home.packages = with pkgs; [
-    zsh-autosuggestions
-    zsh-completions
-  ];
 }
