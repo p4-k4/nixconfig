fd90418184c98c3c97b149ff51cb0ab6ac957191
p4-k4 <p424p424@gmail.com>
2024-12-13 22:09:48 +1300

    feat: revert syntax highlight for AsyncResult

--- config/nvim/lua/plugins/lsp-config.lua
+++ config/nvim/lua/plugins/lsp-config.lua
@@ -20,23 +20,6 @@ return {
       -- LSP servers are installed and managed by Nix
       servers = {
         -- Languages
-        lua_ls = {
-          settings = {
-            Lua = {
-              workspace = { 
-                checkThirdParty = false,
-                library = {
-                  [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-                  [vim.fn.stdpath("config") .. "/lua"] = true,
-                },
-              },
-              telemetry = { enable = false },
-              diagnostics = {
-                globals = { "vim" },
-              },
-            },
-          },
-        },
         nil_ls = {}, -- Nix
         tsserver = {},
         html = {},
@@ -57,13 +40,11 @@ return {
     config = function(_, opts)
       -- Setup diagnostics
       vim.diagnostic.config(opts.diagnostics)
-
       -- Setup keymaps
       local on_attach = function(_, bufnr)
         local map = function(keys, func, desc)
           vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
         end
-
         map("gd", vim.lsp.buf.definition, "Go to Definition")
         map("gr", vim.lsp.buf.references, "Go to References")
         map("gD", vim.lsp.buf.declaration, "Go to Declaration")
@@ -73,7 +54,6 @@ return {
         map("<leader>ca", vim.lsp.buf.code_action, "Code Action")
         map("<leader>rn", vim.lsp.buf.rename, "Rename")
       end
-
       -- Setup capabilities
       local capabilities = vim.tbl_deep_extend(
         "force",
@@ -82,14 +62,12 @@ return {
         require("cmp_nvim_lsp").default_capabilities(),
         opts.capabilities or {}
       )
-
       -- Setup each LSP server
       for server, server_opts in pairs(opts.servers) do
         local final_opts = vim.tbl_deep_extend("force", {
           capabilities = vim.deepcopy(capabilities),
           on_attach = on_attach,
         }, server_opts or {})
-
         require("lspconfig")[server].setup(final_opts)
       end
     end,
